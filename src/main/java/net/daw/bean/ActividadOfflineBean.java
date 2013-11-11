f115206d1ecde4b8bc59c9d92d221a5f6d74baf3
mati <mati@s09-pc05>
2013-11-11 10:31:43 +0100

    Creacion del pojo "ActividadOfflineBean" en net.daw.bean

---
+++ src/main/java/net/daw/bean/ActividadOfflineBean.java
@@ -0,0 +1,109 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author mati
+ */
+public class ActividadOfflineBean {
+    
+     private int id = 0;
+     private String enunciado = "";
+     private Date fecha;
+     private double calificacion = 0;
+     private int  evaluacion =0;
+     private boolean activo = true;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the enunciado
+     */
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    /**
+     * @param enunciado the enunciado to set
+     */
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the calificacion
+     */
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    /**
+     * @param calificacion the calificacion to set
+     */
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    /**
+     * @return the evaluacion
+     */
+    public int getEvaluacion() {
+        return evaluacion;
+    }
+
+    /**
+     * @param evaluacion the evaluacion to set
+     */
+    public void setEvaluacion(int evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    /**
+     * @return the activo
+     */
+    public boolean isActivo() {
+        return activo;
+    }
+
+    /**
+     * @param activo the activo to set
+     */
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+     
+    
+    
+    
+}
